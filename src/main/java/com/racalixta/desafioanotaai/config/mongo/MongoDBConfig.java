c0e8519f9f062a11a30471d01aad24c8a5aeabff
racalixta <ra.calixta@gmail.com>
2024-02-07 20:11:27 -0300

    Configure MongoDB

---
+++ src/main/java/com/racalixta/desafioanotaai/config/mongo/MongoDBConfig.java
@@ -0,0 +1,22 @@
+package com.racalixta.desafioanotaai.config.mongo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
+public class MongoDBConfig {
+
+	@Bean
+	MongoDatabaseFactory mongoConfigure() {
+		return new SimpleMongoClientDatabaseFactory("mongodb://localhost:27017/product-catalog");
+	}
+	
+	@Bean
+	MongoTemplate mongoTemplate() {
+		return new MongoTemplate(mongoConfigure());
+	}
+	
+}
