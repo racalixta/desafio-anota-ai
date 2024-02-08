0e73349a9d2083f63db66b8a6130cb387e0f94f0
racalixta <ra.calixta@gmail.com>
2024-02-08 20:32:29 -0300

    Create Category Not Found Exception

---
+++ src/main/java/com/racalixta/desafioanotaai/domain/category/exception/CategoryNotFoundException.java
@@ -0,0 +1,6 @@
+package com.racalixta.desafioanotaai.domain.category.exception;
+
+public class CategoryNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+}
