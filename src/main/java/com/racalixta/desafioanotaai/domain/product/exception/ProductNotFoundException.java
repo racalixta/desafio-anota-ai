09acb86f1c4a5f625eb35ad7cb011ba1967c3a81
racalixta <ra.calixta@gmail.com>
2024-02-09 18:10:32 -0300

    Create Product Not Found Exception

---
+++ src/main/java/com/racalixta/desafioanotaai/domain/product/exception/ProductNotFoundException.java
@@ -0,0 +1,6 @@
+package com.racalixta.desafioanotaai.domain.product.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+}
